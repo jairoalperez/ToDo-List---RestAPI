6d458fbd0144572c0abe8ec766b2c5cb93c30d4e
Mini-PC\Jairo Perez <jairoalperezprof@gmail.com>
2024-11-20 11:36:34 -0500

    created base on the controller

---
+++ Controllers/TaskController.cs
@@ -0,0 +1,60 @@
+using System.Collections;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList_RestAPI.Models;
+using ToDoList_RestAPI.Services;
+using Task = ToDoList_RestAPI.Models.Task;
+
+namespace ToDoList_RestAPI.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ToDoListController : ControllerBase
+{
+    [HttpGet("all")]
+    public ActionResult<IEnumerable<Task>> GetAllTasks()
+    {
+        return Ok();
+    }
+
+    [HttpGet("user/{userId}")]
+    public ActionResult<IEnumerable<Task>> GetUserTasks([FromRoute] int UserId)
+    {
+        return Ok();
+    }
+
+    [HttpGet("{taskId}")]
+    public ActionResult<Task> GetTask([FromRoute] int TaskId)
+    {
+        return Ok();
+    }
+
+    [HttpPost("create")]
+    public ActionResult<Task> PostTask()
+    {
+        return Ok();
+    }
+
+    [HttpPut("edit/{taskId}")]
+    public ActionResult<Task> PutTask([FromRoute] int taskId)
+    {
+        return Ok();
+    }
+
+    [HttpDelete("delete/all")]
+    public ActionResult<IEnumerable<Task>> DeleteAllTasks()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("delete/user/{userId}")]
+    public ActionResult<IEnumerable<Task>> DeleteUserTasks()
+    {
+        return Ok();
+    }
+
+    [HttpDelete("delete/{task}")]
+    public ActionResult<Task> DeleteTask()
+    {
+        return Ok();
+    }
+}
