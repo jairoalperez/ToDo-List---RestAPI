ae767335c640e2fcff1f824ede055a0781523f55
Jairoalperez <jairoalberto98@hotmail.com>
2025-01-14 09:17:16 -0500

    set up the controller, ready to start implementing endpoints

--- Controllers/UserController.cs
+++ Controllers/UserController.cs
@@ -2,11 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
-namespace ToDoList_RestAPI.Controllers
+namespace ToDoList_RestAPI.Controllers;
+
+[ApiController]
+[Route("api/users")]
+
+public class UserController : ControllerBase
 {
-    public class UserController
+    private readonly AppDbContext _context;
+
+    public UserController(AppDbContext context)
     {
-        
+        _context = context;
     }
+
+
 }
